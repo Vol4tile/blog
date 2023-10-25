21958f58030a511af4565cd8ded866bee0fdd910
Vol4tile <byvolatile@gmail.com>
2023-10-25 18:55:17 +0300

    v0.0.1

--- Blog/src/pages/MainPage.jsx
+++ Blog/src/pages/MainPage.jsx
@@ -1,62 +1,74 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import MainCSS from "../css/Main.module.css";
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import changeMetaTags from "../utils/changeMetaTags";
-import { useEffect } from "react";
-import { allPosts } from "../assets/posts";
+import photo from "../assets/ilkanErdogan.png";
+
 const MainPage = () => {
-  const numberOPosts = 3;
-  const lastPosts = allPosts.slice(-numberOPosts);
+  const [developerText, setDeveloperText] = useState("");
+  const developerTextOptions = [
+    "Full Stack Developer",
+    "Front End Developer",
+    "Back End Developer",
+  ];
+  const [charIndex, setCharIndex] = useState(0);
+  const [textIndex, setTextIndex] = useState(0);
+  const imgControls = useAnimation(); // Resim için animasyon kontrolü
+  const infoControls = useAnimation(); // Bilgi divi için animasyon kontrolü
+
   useEffect(() => {
     changeMetaTags({ title: "Anasayfa" });
   }, []);
+
+  // Belirli bir süre sonra metni değiştir
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (charIndex <= developerTextOptions[textIndex].length) {
+        setCharIndex((prevIndex) => prevIndex + 1);
+        setDeveloperText((prevText) =>
+          developerTextOptions[textIndex].substring(0, charIndex)
+        );
+      } else {
+        setTimeout(() => {
+          setCharIndex(0);
+          setTextIndex(
+            (prevTextIndex) => (prevTextIndex + 1) % developerTextOptions.length
+          );
+          setDeveloperText("");
+        }, 1000);
+      }
+    }, 100);
+    return () => clearInterval(interval);
+  }, [charIndex, textIndex]);
+
   return (
     <>
       <main>
-        {lastPosts.map((post) => {
-          return (
-            <motion.article
-              className={MainCSS.article}
-              key={post.id}
-              animate={{ opacity: 1 }}
-              initial={{ opacity: 0 }}
-              exit={{ opacity: 0 }}
-            >
-              <div className={MainCSS.postImage}>
-                <img src={post.paylasimResmi} alt="React logosu" />
-              </div>
-              <div className={MainCSS.date}>
-                <div>{post.tarih}</div>
-                <div className={MainCSS.tag}>{post.etiket}</div>
-              </div>
-              <h1>{post.baslik}</h1>
-              <p>{post.metin.substring(0, 450)}...</p>
-              <div className={MainCSS.bottomContent}>
-                <div className={MainCSS.writer}>
-                  <div className={MainCSS.writerImage}>
-                    <img src={post.paylasanResmi} alt={post.paylasanAdi} />
-                  </div>
-                  <div className={MainCSS.writerInfo}>
-                    <div className={MainCSS.writerName}>{post.paylasanAdi}</div>
-                    <div className={MainCSS.writerTitle}>{post.paylasanUnvan}</div>
-                  </div>
-                </div>
-                <div
-                  style={{
-                    opacity: "0.8",
-                    display: "flex",
-                    alignItems: "center",
-                  }}
-                >
-                  <Link to="Post" state={{ post }}>
-                    Devamını gör
-                  </Link>
-                </div>
-              </div>
-            </motion.article>
-          );
-        })}
+        <motion.article
+          animate={{ opacity: 1 }}
+          initial={{ opacity: 0 }}
+          exit={{ opacity: 0 }}
+          className={MainCSS.article}
+        >
+          <div className={MainCSS.me}>
+            <motion.img
+              src={photo}
+              alt="İlkan Erdoğan"
+              className={MainCSS.img}
+              animate={imgControls} // Resim için animasyon kontrolünü kullan
+            />
+            <div className={MainCSS.info}>
+              <motion.div
+                className={MainCSS.name}
+                animate={infoControls} // Bilgi divi için animasyon kontrolünü kullan
+              >
+                İlkan ERDOĞAN
+              </motion.div>
+              <div className={MainCSS.title}>{developerText}|</div>
+            </div>
+          </div>
+          <div></div>
+        </motion.article>
       </main>
     </>
   );
