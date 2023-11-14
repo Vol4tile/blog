cf28b824558efbdec71f0fc4bf5dbbd2a31a5b34
Vol4tile <byvolatile@gmail.com>
2023-11-15 00:18:56 +0300

    v1.4

--- Blog/src/assets/posts.js
+++ Blog/src/assets/posts.js
@@ -14,7 +14,7 @@ export const allPosts = [
 }
 </pre>`,
   },
-  
+
   {
     id: 1,
 
@@ -64,5 +64,47 @@ export const allPosts = [
      }
      </pre><p>Yukarıdaki örnekte, useEffect kullanarak bileşen monte edildiğinde fetchData fonksiyonunu çağırıyoruz.</p><p>Bu iki hook'u kullanarak, fonksiyonel bileşenlerde state yönetimini ve yaşam döngüsü olaylarına müdahale etmeyi kolaylaştırabilirsiniz.</p><p><br></p>`,
   },
-  
+
+  {
+    id: 2,
+
+    tarih: "30 Mart 2023",
+    etiket: "Teknoloji",
+    baslik: " React derinlemesi useState  ",
+    metin: `
+    <h2>useEffect</h2><p> React'ta yan etkileri (side effects) yönetmek için kullanılan bir Hook'tur. Yan etkiler, örneğin ağ istekleri yapma, veritabanı sorgularını işleme, abonelikleri yönetme gibi işlemleri ifade eder. Bu tür işlemler
+    genellikle bir bileşenin yaşam döngüsü içinde gerçekleşir.</p><p>useEffect, bir işlev ve bağımlılıklar (dependencies) listesi alır. Bu işlev, bileşen her render edildiğinde çalışır. Ancak, bağımlılıklar listesi verildiğinde, sadece bu bağımlılıkların değerleri değiştiğinde veya bileşen ilk kez render edildiğinde çalışır.</p><p>İşte basit bir örnek:</p><pre class="ql-syntax" spellcheck="false"><span class="hljs-keyword">import</span> <span class="hljs-title class_">React</span>, { useState, useEffect } <span class="hljs-keyword">from</span> <span class="hljs-string">'react'</span>;
+
+    <span class="hljs-keyword">const</span> <span class="hljs-title function_">ExampleComponent</span> = () =&gt; {
+      <span class="hljs-comment">// State tanımla</span>
+      <span class="hljs-keyword">const</span> [count, setCount] = <span class="hljs-title function_">useState</span>(<span class="hljs-number">0</span>);
+    
+      <span class="hljs-comment">// useEffect kullanımı</span>
+      <span class="hljs-title function_">useEffect</span>(<span class="hljs-function">() =&gt;</span> {
+        <span class="hljs-comment">// Bu kısım, bileşen her render edildiğinde çalışır</span>
+        <span class="hljs-variable language_">console</span>.<span class="hljs-title function_">log</span>(<span class="hljs-string">'Component rendered'</span>);
+    
+        <span class="hljs-comment">// Temizleme fonksiyonu (clean-up function)</span>
+        <span class="hljs-keyword">return</span> <span class="hljs-function">() =&gt;</span> {
+          <span class="hljs-variable language_">console</span>.<span class="hljs-title function_">log</span>(<span class="hljs-string">'Component will unmount or dependencies changed'</span>);
+        };
+      }, [<span class="hljs-comment">/* dependencies */</span>]);
+    
+      <span class="hljs-comment">// State'i güncelleme</span>
+      <span class="hljs-keyword">const</span> <span class="hljs-title function_">increment</span> = () =&gt; {
+        <span class="hljs-title function_">setCount</span>(count + <span class="hljs-number">1</span>);
+      };
+    
+      <span class="hljs-keyword">return</span> (
+        <span class="hljs-tag">&lt;<span class="hljs-name">div</span>&gt;</span>
+          <span class="hljs-tag">&lt;<span class="hljs-name">p</span>&gt;</span>Count: {count}<span class="hljs-tag">&lt;/<span class="hljs-name">p</span>&gt;</span>
+          <span class="hljs-tag">&lt;<span class="hljs-name">button</span> <span class="hljs-attr">onClick</span>=<span class="hljs-string">{increment}</span>&gt;</span>Increment<span class="hljs-tag">&lt;/<span class="hljs-name">button</span>&gt;</span>
+        <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+      );
+    };
+    
+    <span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> <span class="hljs-title class_">ExampleComponent</span>;
+    </pre><p><br></p><p>Bu örnekte, useEffect içindeki fonksiyon, her render işlemi sonrasında çalışacaktır. Eğer boş bir bağımlılıklar listesi bırakırsanız ([]), bu efekt sadece bileşenin monte edilme ve demonte edilme anlarında çalışacaktır. Bağımlılıklar listesine özel bir state eklerseniz, bu efekt sadece o state değiştiğinde çalışacaktır.</p><p>Temizleme fonksiyonu, bileşen demonte edildiğinde veya bağımlılıklar değiştiğinde çalışır. Bu, örneğin abonelikleri veya zamanlayıcıları temizleme gibi işlemler için kullanışlıdır.</p>
+    `,
+  },
 ];
